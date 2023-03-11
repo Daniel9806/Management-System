56f8037bfc730c6246299f6602d0793d8dd08449
Daniel <80117423+Daniel9806@users.noreply.github.com>
2023-03-11 11:47:57 -0500

    add modals

---
+++ src/components/tools/ModalStatic.vue
@@ -0,0 +1,65 @@
+<template>
+  <div
+    class="fixed right-0 left-0 top-0 bottom-0 bg-opacity-60 bg-gray-300 z-40"
+  >
+    <transition
+      appear
+      :enter-active-class="'animated ' + animate"
+      leave-active-class="animated rotateOutDownLeft"
+    >
+      <div
+        :style="'width: ' + maxWidth + 'px'"
+        class="relative bg-gray-100 dark:bg-gray-700 mx-auto h-auto mt-20 rounded"
+      >
+        <div
+          class="flex items-center bg-blue-400 dark:bg-blue-500 h-9 justify-between rounded-t"
+        >
+          <p class="ml-2 font-medium text-lg text-black">{{ title }}</p>
+          <!-- <span
+            @click="clickOn"
+            class="p-1 cursor-pointer material-icons text-black hover:bg-gray-400 hover:bg-opacity-30 rounded-full"
+            >close</span
+          > -->
+        </div>
+        <div class="p-2">
+          <slot></slot>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalGeneric",
+
+//   emits: ["closeModal"],
+
+  props: {
+    title: {
+      type: String,
+      default: "Title",
+    },
+    maxWidth: {
+      type: String,
+      default: "800",
+    },
+    animate: {
+      type: String,
+      default: "fadeIn",
+    },
+    // mTop: {
+    //   type: Number,
+    //   default: "20",
+    // },
+  },
+
+  setup(_, context) {
+    return {
+    //   clickOn: () => {
+    //     context.emit("closeModal");
+    //   },
+    };
+  },
+};
+</script>
---
+++ src/components/tools/ModalUserProfile.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    @click.self="clickOn"
+    class="fixed right-0 left-0 top-0 bottom-0 bg-opacity-60 bg-gray-300 z-40"
+  >
+    <transition
+      appear
+      enter-active-class="animated slideInRight faster"
+      leave-active-class="animated slideOutRight"
+    >
+      <div
+        :style="'width: ' + maxWidth + 'px'"
+        class="absolute bg-gray-100 dark:bg-gray-700 right-0 h-screen"
+      >
+        <div
+          class="flex items-center bg-blue-300 dark:bg-blue-400 h-9 justify-between"
+        >
+          <p class="ml-2 font-medium text-lg text-black">{{ title }}</p>
+          <!-- <span
+            @click="clickOn"
+            class="p-1 cursor-pointer material-icons text-black hover:bg-gray-400 hover:bg-opacity-30 rounded-full"
+            >close</span
+          > -->
+        </div>
+        <div class="p-2">
+          <slot></slot>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalGeneric",
+
+  emits: ["closeModal"],
+
+  props: {
+    title: {
+      type: String,
+      default: "Perfil de Usuario",
+    },
+    maxWidth: {
+      type: String,
+      default: "350",
+    },
+    
+  },
+
+  setup(_, context) {
+    return {
+      clickOn: () => {
+        context.emit("closeModal");
+      },
+    };
+  },
+};
+</script>
