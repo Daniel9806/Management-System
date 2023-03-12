5e1b8bf8bae9b7859fc75a4f3bcc506d8ad42000
Daniel <80117423+Daniel9806@users.noreply.github.com>
2023-03-12 10:47:58 -0500

    Update UserView.vue

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,15 +1,27 @@
 <template>
-    <div class="">
+    <div class="space-y-1">
+        <button @click="modalActive = true" class="flex items-center p-2 bg-main-color rounded-md text-sm">New User
+            <i class="material-icons ml-1">add</i>
+        </button>
         <Loading v-if="userStore.getLoadingUser" />
 
-        <TableGeneric v-else :filteredList="users" :header="header" :fields="fields" 
-        :greenAction="'Edit'" :blueAction="'Details'" :redAction="'Delete'" 
-        @onGreenAction="onEdit($event)" />
+        <TableGeneric v-else :filteredList="users" :header="header" :fields="fields" :greenAction="'Edit'"
+            :blueAction="'Details'" :redAction="'Delete'" @onGreenAction="onEdit($event)" />
+
+        <Teleport to="#modal">
+            <Transition name="modal"> 
+                <modal-generic v-if="modalActive" 
+                @closeModal="modalActive = false" maxWidth="600" title="New User">
+                    ASASASASA
+                </modal-generic>
+            </Transition>
+        </Teleport>
     </div>
 </template>
 
 <script setup>
 import TableGeneric from '../components/tools/TableGeneric.vue'
+import ModalGeneric from '../components/tools/ModalGeneric.vue'
 import { reactive, ref, onMounted } from 'vue'
 import { useUserStore } from '../store/userStore.js'
 import Loading from '../components/tools/Loading.vue'
@@ -17,6 +29,7 @@ import Loading from '../components/tools/Loading.vue'
 const userStore = useUserStore()
 
 const users = ref([])
+const modalActive = ref(false)
 
 onMounted(async () => {
     users.value = await userStore.fetchUsers()
@@ -36,4 +49,6 @@ const onEdit = (item) => {
 
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
