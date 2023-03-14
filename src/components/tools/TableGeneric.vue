0cedfccffa1c59db4b89aa04d3462fb7b3b9dd12
Daniel <80117423+Daniel9806@users.noreply.github.com>
2023-03-13 19:18:16 -0500

    Update TableGeneric.vue

--- src/components/tools/TableGeneric.vue
+++ src/components/tools/TableGeneric.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left">
+  <div class="overflow-x-auto shadow-md">
+    <table class="dark:bg-neutral-900 bg-slate-500 w-full text-sm text-left rounded-md">
       <thead class="text-base text-black dark:text-white border-b border-gray-600
        dark:border-black">
         <tr>
@@ -11,22 +11,32 @@
         </tr>
       </thead>
       <tbody>
-        <!-- Loop through the list get the each student data -->
-        <tr class="border-t border-gray-400 dark:bg-neutral-800 dark:border-black
-           hover:bg-gray-100 dark:hover:bg-neutral-700" v-for="item in filteredList" :key="item">
-          <td class="py-3 px-6" v-for="field in fields" :key="field">
-            {{ item[field] }}
+        <tr v-for="item in filteredList" :key="item" 
+        class="border-t border-gray-400 bg-slate-300 dark:bg-neutral-700 
+        dark:border-black hover:bg-gray-100 dark:hover:bg-neutral-600" >
+          <td class="w-1/5 px-6" v-for="field in fields" :key="field">
+            <template v-if="field != 'confirmed'">
+              {{ item[field] }}
+            </template>
+            <div v-else class="ml-6">
+              <template v-if="item[field]">
+                <i class="text-green-600 material-icons">checked</i>
+              </template>
+              <template v-else>
+                <i class="text-red-500 material-icons">close</i>
+              </template>
+            </div>
           </td>
           <!-- ACTIONS -->
-          <td class="py-2.5 px-6 space-x-2">
+          <td class="py-2 mr-4 flex flex-row space-x-2">
             <span v-if="blueAction" @click="$emit('onBlueAction', item)"
-              class="cursor-pointer font-medium text-blue-400 dark:text-blue-500
+              class="cursor-pointer font-medium text-blue-500
                hover:underline">{{ blueAction }}</span>
             <span v-if="greenAction" @click="$emit('onGreenAction', item)"
-              class="cursor-pointer font-medium text-green-400 dark:text-green-500 hover:underline">{{ greenAction
+              class="cursor-pointer font-medium text-green-500 hover:underline">{{ greenAction
               }}</span>
             <span v-if="redAction" @click="$emit('onRedAction', item)"
-              class="cursor-pointer font-medium text-red-400 dark:text-red-400 hover:underline">{{ redAction }}</span>
+              class="cursor-pointer font-medium text-red-500 hover:underline">{{ redAction }}</span>
           </td>
         </tr>
       </tbody>
