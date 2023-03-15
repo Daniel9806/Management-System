393d4fa8b5381eb929b2448e783660b56708bdec
Daniel <80117423+Daniel9806@users.noreply.github.com>
2023-03-15 14:32:06 -0500

    adding composables

---
+++ src/composables/useAlert.js
@@ -0,0 +1,57 @@
+import Swal from "sweetalert2";
+
+//Alert Icons
+//success
+//error
+//warning
+//info
+//question
+
+const useAlert = () => {
+  const timerToast = Swal.mixin({
+    toast: true,
+    icon: 'success',
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+  const timerAlert = Swal.mixin({
+    toast: false,
+    position: "top",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+  const confirmAlert = Swal.mixin({
+    showCancelButton: true,
+    cancelButtonText: "Cancelar",
+    confirmButtonText: "Si",
+    reverseButtons: true,
+  });
+
+  const okAlert = Swal.mixin({
+    showCancelButton: false,
+    confirmButtonText: "OK",
+  });
+ 
+  
+  return {
+    timerToast,
+    timerAlert,
+    confirmAlert,
+    okAlert
+  };
+};
+
+export default useAlert;
