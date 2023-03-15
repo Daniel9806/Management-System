4059ea8ba089cc45c133e126453b53f73969d531
Daniel <80117423+Daniel9806@users.noreply.github.com>
2023-03-14 22:25:44 -0500

    Update TopBar.vue

--- src/components/navegation/TopBar.vue
+++ src/components/navegation/TopBar.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="absolute w-9/12 mr-1 top-5 right-5 rounded-xl h-11 bg-slate-300 dark:bg-neutral-700">
-    <div class="flex h-11 justify-between">
-      <div><i class="material-icons cursor-pointer ml-2 mt-2">menu</i></div>
-      <div class="flex items-center space-x-2 mr-4 cursor-pointer">
-        <DarkMode />
-        <img class="w-8 h-8 rounded-full" src="../../assets/vue.svg" alt="">
-        <p @click="modalUserActive = true" 
-        v-if="authStore.getUserAuth">{{ authStore.getUserAuth.username }}</p>
-      </div>
+  <div class="absolute w-48 top-7 right-0 h-11">
+    <div class="flex items-center space-x-2 mr-4 cursor-pointer">
+      <DarkMode />
+      <img class="w-8 h-8 rounded-full" src="../../assets/vue.svg" alt="">
+      <p @click="modalUserActive = true" v-if="authStore.getUserAuth">{{ authStore.getUserAuth.username }}</p>
     </div>
   </div>
 
-  <modal-user v-if="modalUserActive" @closeModal="modalUserActive = false" 
-  :title="'User Settings'">
+  <modal-user v-if="modalUserActive" @closeModal="modalUserActive = false" :title="'User Settings'">
     <UserProfile />
   </modal-user>
 </template>
@@ -30,6 +25,4 @@ const modalUserActive = ref(false)
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
