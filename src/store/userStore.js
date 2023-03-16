333327e90c4c109fd435e572bfa68320190ca771
Daniel <80117423+Daniel9806@users.noreply.github.com>
2023-03-16 10:22:35 -0500

    Update userStore.js

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,5 +1,8 @@
 import { defineStore } from 'pinia'
 import mainApi from '../api/mainApi'
+import { useAlert } from '../composables/useAlert'
+
+const { timerToast } = useAlert()
 
 export const useUserStore = defineStore('userStore', {
 
@@ -29,21 +32,42 @@ export const useUserStore = defineStore('userStore', {
             } catch (error) {
                 this.loadingFetching = false
                 console.log(error)
-                return error
+                timerToast.fire({
+                    icon: 'error',
+                    text: `${error.message}`
+                })
             }
         },
 
         async createUser(user) {
             this.loadingCreating = true
-            console.log(user)
             try {
                 const { data } = await mainApi.post('/users', user)
-                const newUser = JSON.parse(data.data)
-                console.log(newUser)
-                this.loadingCreating = false
+                console.log(data)
+                if (data.message == "validation.unique") {
+                    console.log('error de validacion')
+                    this.loadingCreating = false
+                    timerToast.fire({
+                        icon: 'error',
+                        text: `${data.message}`
+                    })
+                } else {
+                    const newUser = JSON.parse(data.data)
+                    console.log(newUser)
+                    this.loadingCreating = false
+                    timerToast.fire({
+                        icon: 'success',
+                        text: 'Usuario registrdo'
+                    })
+                    return newUser
+                }
             } catch (error) {
                 this.loadingCreating = false
                 console.log(error)
+                timerToast.fire({
+                    icon: 'error',
+                    text: `${error.message}`
+                })
             }
         }
     }
